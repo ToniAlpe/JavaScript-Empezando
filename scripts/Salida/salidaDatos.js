16b8a8a175ea7505ddbdaa52ced7b04042e5f687
ToniAlpe <242182314+ToniAlpe@users.noreply.github.com>
2026-02-10 10:32:20 +0100

    Asamblea salida de datos

--- scripts/Salida/salidaDatos.js
+++ scripts/Salida/salidaDatos.js
@@ -9,5 +9,10 @@ console.warn("ALERTA, ALERTA!")
 console.error("Ya has roto algo")
 //Mensaje de error (se muestra con un icono rojo).
 
+/* NO FUNCIONA
 document.write("Hola mundo");
-document.write("<h1>Hola mundo</h1>");
+document.write("<h1>Hola mundo</h1>");*/
+
+// SI FUNCIONA
+let nombre = "German"
+document.body.innerHTML += `Hola mundo ${nombre}`;
