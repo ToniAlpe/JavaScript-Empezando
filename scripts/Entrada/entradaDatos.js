0727d62223ab5c8ae81a75a25fab00b95b176c60
ToniAlpe <242182314+ToniAlpe@users.noreply.github.com>
2026-02-10 10:19:07 +0100

    Carpetas hechas y enlazadas al html

---
+++ scripts/Entrada/entradaDatos.js
@@ -0,0 +1,5 @@
+let nombre = prompt("¿Cual es su nombre?: ")
+console.log(nombre);
+
+let respuesta = confirm("¿Has estudiado para el examen?")
+console.log(respuesta);
---
+++ scripts/Salida/salidaDatos.js
@@ -0,0 +1,13 @@
+console.log("HoliwiUWU");
+
+console.info("Soy INFO")
+//Mensaje informativo.
+console.debug("Debuggeando")
+//Mensaje de depuración (puede estar oculto por defecto).
+console.warn("ALERTA, ALERTA!")
+//Mensaje de advertencia (se muestra con un icono amarillo).
+console.error("Ya has roto algo")
+//Mensaje de error (se muestra con un icono rojo).
+
+document.write("Hola mundo");
+document.write("<h1>Hola mundo</h1>");
