afd7011723ee086514d5f771d56c93e9786ac586
ToniAlpe <242182314+ToniAlpe@users.noreply.github.com>
2026-03-10 09:40:13 +0100

    interaccion usuario

---
+++ scripts/interaccionUsuario.js
@@ -0,0 +1,6 @@
+//Para acceder a todos los h1.
+const encabezados=document.getElementsByTagName("h1");
+//Acceder a la ID noticias.
+const noticias=document.getElementById("noticias");
+//Acceder a la clase articulo.
+const articulos=document.getElementsByClassName("articulo");
