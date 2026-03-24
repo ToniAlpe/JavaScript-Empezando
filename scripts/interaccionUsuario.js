e5ea0bd70b81a18b7c4baedb861748fad3302058
ToniAlpe <242182314+ToniAlpe@users.noreply.github.com>
2026-03-24 08:29:28 +0100

    Explicacion

--- scripts/interaccionUsuario.js
+++ scripts/interaccionUsuario.js
@@ -4,3 +4,7 @@ const encabezados=document.getElementsByTagName("h1");
 const noticias=document.getElementById("noticias");
 //Acceder a la clase articulo.
 const articulos=document.getElementsByClassName("articulo");
+
+//innerHTML --> Muestra el contenedor y el contenido
+//innerText --> Muestra texto visible
+//textContent --> Muestra texto visible e invisible
